16f0d0db75c3e91c10fca842cc1cecdd52e9da0b
Carlos Matheus <carlos.matheus@outlook.com>
2023-02-17 20:35:15 -0300

    Program.cs atualizado
    Códigos iniciais do aplicativo:
      - Inseridas algumas matemáticas básicas de inteiros em C#.
      - Inserido código para criar um aplicativo de calculadora básico.
      - Depurado o aplicativo para localizar e corrigir erros.
    
    TO DO: Refinar o código para torná-lo mais eficiente.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿// Declare variables and then initialize to zero
+int num1 = 0;
+int num2 = 0;
+
+// Display title as the C# console calculator app
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+// Ask the user to type the first number
+Console.WriteLine("Type a number, and then press <Enter>");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number
+Console.WriteLine("Type another number, and then press <Enter>");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to choose an option
+Console.WriteLine("Choose an option from the followin list:");
+Console.WriteLine("\t[a] - Add");
+Console.WriteLine("\t[s] - Subtract");
+Console.WriteLine("\t[m] - Multiply");
+Console.WriteLine("\t[d] - Divide");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} x {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} : {num2} = " + (num1 / num2));
+        break;
+}
+
+// Wait for the user to respond before closing
+Console.Write("Press any key to close the Calculator console app...\n");
+Console.ReadKey();

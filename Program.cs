023c4854aa5f79f8612f3d8982134b67a9794768
Carlos Matheus <carlos.matheus@outlook.com>
2023-02-18 15:26:39 -0300

    Erro "divisão por zero"
    Implementada uma exceção na opção divisão que, até que insira um número diferente de zero, será exibida uma mensagem.

--- Program.cs
+++ Program.cs
@@ -35,8 +35,15 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} x {num2} = " + (num1 * num2));
         break;
     case "d":
+        // Ask the user to enter a non-zero divisor until they do so
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero dividor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} : {num2} = " + (num1 / num2));
         break;
+    // end case
 }
 
 // Wait for the user to respond before closing
